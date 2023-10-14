01d61c44cc5961ec06354fb63a5f28727357a0df
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-10-14 17:27:48 +0100

    added function to validate words

--- myScript.js
+++ myScript.js
@@ -7,6 +7,7 @@ const word_URL = "https://words.dev-apis.com/word-of-the-day"
 let word = ""
 let winningWord = ""
 const maxLength = 5;
+let currentGuess =1 
 let currentLetter = "";
 
 
@@ -25,6 +26,30 @@ getWord().then(result =>{
     winningWord = result;
 })
 
+const asyncPostCall = async (str) => {
+    try {
+        const response = await fetch('https://words.dev-apis.com/validate-word', {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json'
+           },
+           body: JSON.stringify({
+   
+             word: str,
+            })
+         });
+         const data = await response.json();
+      
+         console.log(data.validWord);
+         return data.validWord;
+       } catch(error) {
+
+          console.log(error)
+         } 
+    }
+
+  
+
 
 
 // Working on this section -----------------------------
@@ -39,9 +64,13 @@ function handleEnter(array){
         word=""
     }
     compareWords(winningWord, newArr);
-    // console.log(newArr)
 }
 
+function winningAnswer(str1, str2){
+    if(str1 == str2){
+        alert("winner")
+    }
+}
 
 function compareWords(string, array){
     let lettersLeft = string;
@@ -51,12 +80,18 @@ function compareWords(string, array){
             lettersLeft = lettersLeft.replace(array[i].innerHTML, "");
             console.log(lettersLeft)
             array[i].classList.add("winningBox");
+        }else{
+            if(!string.includes(array[i])){
+                array[i].classList.add("greyBox");
+        }
         }
     }
     for(let i=0; i<5; i++){
         if(lettersLeft.includes(array[i].innerHTML)){
             lettersLeft = lettersLeft.replace(array[i].innerHTML, "");
+            array[i].classList.remove("greyBox");
             array[i].classList.add("orangeBox")
+        }else{
         }
     } 
     }
@@ -87,14 +122,35 @@ function enterKeys(array){
 
 
 
+function restart(num){
+    if(num == 7){
+        alert( `you lose the word of the day is ${winningWord}`)
+        location.reload();
+    }
+}
+
 
 function enter(){
     document.addEventListener("keydown", (event)=>{
         if(event.key =="Enter" && word.length == maxLength){
+            validWord();
+        }
+    })
+}
+
+function validWord(){
+    asyncPostCall(word).then(result =>{
+        if(result){
+            currentGuess += 1;
+            restart(currentGuess);
+            winningAnswer(word, winningWord)
             handleEnter(gameBox);
         }
     })
 }
+
+
+    
 
 function removeLetter(array){
     document.addEventListener("keydown",(event)=>{
@@ -117,7 +173,7 @@ function removeLetter(array){
 enterKeys(gameBox);
 enter();
 
-// handleEnter(gameBox);
+
 
 removeLetter(gameBox);
 
